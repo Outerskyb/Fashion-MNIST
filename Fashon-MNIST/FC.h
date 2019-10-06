0c13b95ddf62204614a46e3473d18d1f83b39d7d
Outerskyb <outerskyb@gmail.com>
2019-10-06 18:34:49 +0900

    weight init

--- Fashon-MNIST/FC.h
+++ Fashon-MNIST/FC.h
@@ -6,43 +6,43 @@
 
 
 class FC :
-	public Layer
+    public Layer
 {
 public:
-	enum class ActivationFunction
-	{
-		Relu = 1,
-		SoftMax = 2
-	};
+    enum class ActivationFunction
+    {
+        Relu = 1,
+        SoftMax = 2
+    };
 
 public:
-	FC(ActivationFunction af,int number_of_input,int number_of_node): activation_function(af),number_of_input(number_of_input),number_of_node(number_of_node)
-	{
+    FC(ActivationFunction af, int number_of_input, int number_of_node) : activation_function(af), number_of_input(number_of_input), number_of_node(number_of_node)
+    {
         srand(time(0));
-		weights = cv::Mat(number_of_node, number_of_input, CV_32FC1);
-		bias    = cv::Mat(number_of_node, number_of_input, CV_32FC1);
+        weights = cv::Mat(number_of_node, number_of_input, CV_32FC1);
+        bias = cv::Mat(number_of_node, number_of_input, CV_32FC1);
 
         for (int i = 0; i < number_of_node; i++) {
             for (int j = 0; j < number_of_input; j++) {
-                weights.at<float>(i, j) = rand()+1;
-                bias.at<float>(i, j) = rand()+1;
+                weights.at<float>(i, j) = ((rand() % 2) ? 1 : -1) * rand() / 1000.0 + 1;
+                bias.at<float>(i, j) = ((rand() % 2) ? 1 : -1) * rand() / 1000.0 + 1;
             }
         }
-	}
+    }
 
 private:
-	ActivationFunction activation_function;
-	int number_of_input;
-	int number_of_node;
-	cv::Mat weights;
-	cv::Mat bias;
+    ActivationFunction activation_function;
+    int number_of_input;
+    int number_of_node;
+    cv::Mat weights;
+    cv::Mat bias;
 
 private:
     float relu(float);
     float softmax(float x, cv::Mat result, bool re);
-	
+
 public:
-    	cv::Mat run(cv::Mat);
-	
+    cv::Mat run(cv::Mat);
+
 };
 
