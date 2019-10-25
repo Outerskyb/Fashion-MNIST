df3184ee9706ae2cd76daf0f95ca0be081de1bfb
Outerskyb <outerskyb@gmail.com>
2019-10-25 22:55:15 +0900

    conv

--- Fashon-MNIST/Conv.cpp
+++ Fashon-MNIST/Conv.cpp
@@ -2,8 +2,8 @@
 
 cv::Mat Conv::run(cv::Mat input)
 {
-    int sz[] = { input.rows - filter.rows + 1,input.cols - filter.cols + 1 };
-    cv::Mat result = cv::Mat::zeros(filter.channels()*input.channels(), sz, CV_32FC(filter.channels()));
+    int sz[] = { filter.channels() * input.channels(), input.rows - filter.rows + 1,input.cols - filter.cols + 1 };
+    cv::Mat result = cv::Mat::zeros(filter.channels()*input.channels(), sz, CV_32FC1);
     for (int k = 0; k < input.channels(); k++){
         for (int i = 0; i < input.rows; i++) {
             for (int j = 0; j < input.cols; j++) {
@@ -26,6 +26,32 @@ cv::Mat Conv::run(cv::Mat input)
 
 cv::Mat Conv::train(cv::Mat input, cv::Mat target)
 {
-    return cv::Mat();
+    if (target.rows == 0) {
+        ip = input;
+        return  this->run(input);
+    }
+   /* else {
+        //calculate previous layer's delta
+        //activation`(prev layer's output == curr's input) * sigma(curr's node's weight * curr's node's delta)
+        cv::Mat delta = cv::Mat::zeros(ip.rows, ip.cols, CV_32FC1);
+        for (int k = 0; k < number_of_input; k++) {
+            for (int i = 0; i < number_of_node; i++) {
+                delta.at<float>(k, 0) += weights.at<float>(i, k) * target.at<float>(i, 0);
+            }
+            delta.at<float>(k, 0) *= (ip.at<float>(k, 0) == 0) ? 0 : 1; //relu derivative // *(1 - ip.at<float>(k, 0));
+        }
+
+        for (int k = 0; k < number_of_node; k++) {
+            for (int i = 0; i < ip.rows; i++) {
+                for (int j = 0; j < ip.cols; j++) {
+                    weights.at<float>(k, i * ip.cols + j)
+                        -= etha * ip.at<float>(i, j) * target.at<float>(k, 0);
+                }
+            }
+            weights.at<float>(k, ip.rows * ip.cols) -= etha * target.at<float>(k, 0);
+        }
+        ip = cv::Mat();
+        return delta;
+    }*/
 }
 
--- Fashon-MNIST/Conv.h
+++ Fashon-MNIST/Conv.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Layer.h"
+#include <random>
 class Conv :
     public Layer
 {
@@ -9,12 +10,26 @@ public:
 
     Conv(int filter_height, int filter_width, int filter_channel) {
         //제대로 돌아가는지 확인 필요
-        filter = cv::Mat(cv::Size(filter_width, filter_height), CV_32FC(filter_channel));
+        int sz[] = { filter_channel,filter_height,filter_width };
+        filter = cv::Mat(filter_channel,sz, CV_32FC(filter_channel));
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::normal_distribution<> nd(0, sqrt(6.0 /filter_height/filter_width));
+        srand(time(0));
+
+        for (int k = 0; k < filter_channel; k++) {
+            for (int i = 0; i < filter_height; i++) {
+                for (int j = 0; j < filter_width + 1; j++) {
+                    filter.at<float>(i, j) = ((rand() % 2) ? 1 : -1) * nd(mt);
+                }
+            }
+        }
     }
     
 private:
     cv::Mat filter;
-    
+    cv::Mat ip;
+
 public:
     cv::Mat run(cv::Mat);
     cv::Mat train(cv::Mat input, cv::Mat target);
